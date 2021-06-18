bbcfa3123af40b9137fcd8fcb5ef34a247499b24
roshak7 <roshak7@gmail.com>
2021-06-18 14:11:24 +0300

    first commit

---
+++ extension.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from config import exchanger
+from collections import defaultdict
+
+
+class ConverterException(Exception):
+    pass
+
+
+class UserInfo:
+    def __init__(self):
+        self.f = "RUB"
+        self.t = "USD"
+
+
+class UserDB:
+    def __init__(self):
+        self.db = defaultdict(UserInfo)
+
+    def change_from(self, user_id, val):
+        self.db[user_id].f = val
+
+    def change_to(self, user_id, val):
+        self.db[user_id].t = val
+
+    def get_pair(self, user_id):
+        user = self.db[user_id]
+        return user.f, user.t
+
+
+class Convertor:
+    @staticmethod
+    def get_price(values):
+        if len(values) != 3:
+            raise ConverterException("Неверное количество параметров")
+
+        quote, base, amount = values
+
+        if quote == base:
+            raise ConverterException(f'Невозможно перевести одинаковые валюты {base}')
+
+        try:
+            quote_formatted = exchanger[quote]
+        except KeyError:
+            raise ConverterException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_formatted = exchanger[base]
+        except KeyError:
+            raise ConverterException(f'Не удалось обработать команду {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConverterException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(
+            f'https://free.currconv.com/api/v7/convert?q={quote_formatted}_{base_formatted}&compact=ultra&apiKey=d36a50e14b5d1499e823')
+
+        result = float(json.loads(r.content)[f"{quote_formatted}_{base_formatted}"]) * amount
+
+        return round(result, 3)
